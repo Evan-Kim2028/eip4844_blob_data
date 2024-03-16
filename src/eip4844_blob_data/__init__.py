8e89feae8b28fc81ba40bd7b1c9b5798ad245e5a
Evan K <ekcopersonal@gmail.com>
2024-03-16 18:35:19 -0400

    feat: add panel class to manage dashboard charts

---
+++ src/eip4844_blob_data/__init__.py
@@ -0,0 +1,2 @@
+def hello() -> str:
+    return "Hello from eip4844-blob-data!"
---
+++ src/eip4844_blob_data/panels.py
@@ -0,0 +1,53 @@
+import panel as pn
+import polars as pl
+from dataclasses import dataclass
+from ethpandaops_python.client import Queries
+
+pn.extension("plotly", template="material", sizing_mode="stretch_width")
+
+
+@dataclass
+class Panels:
+
+    def blob_propagation(self):
+        clickhouse_queries = Queries()
+
+        # make the query
+        blob_propagation_query = clickhouse_queries.blob_propagation(
+            time="7", network="mainnet"
+        )
+        blob_prop_df: pl.DataFrame = pl.from_pandas(blob_propagation_query)
+
+        # make the panel
+        blob_prop_panel = (
+            blob_prop_df.group_by("slot")
+            .agg(
+                pl.col("slot_start_date_time").first(),
+                pl.col("propagation_slot_start_diff").mean().alias(
+                    "prop_slot_time_diff_mean"),
+                pl.col("propagation_slot_start_diff").min().alias(
+                    "prop_slot_time_diff_min"),
+                pl.col("propagation_slot_start_diff").max().alias(
+                    "prop_slot_time_diff_max"),
+            )
+            .filter(pl.col("prop_slot_time_diff_max") < 15000)
+            .sort(by="slot_start_date_time")
+            .plot.line(
+                x="slot_start_date_time",
+                y=[
+                    "prop_slot_time_diff_min",
+                    "prop_slot_time_diff_mean",
+                    "prop_slot_time_diff_max",
+                ],
+                xlabel="Date",
+                ylabel="Time (ms)",
+                line_width=2,
+                alpha=0.8,
+            )
+        )
+
+        return pn.Column(
+            "# Mainnet Blob Sidecar Propagation Time",
+            blob_prop_panel,
+            sizing_mode="stretch_width",
+        )
